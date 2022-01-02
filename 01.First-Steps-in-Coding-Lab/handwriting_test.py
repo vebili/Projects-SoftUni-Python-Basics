ffe928bac3d26155e9b108d6c11119391e13e511
vebili <74007431+vebili@users.noreply.github.com>
2022-01-02 09:48:28 +0200

    Up

--- 01.First-Steps-in-Coding-Lab/handwriting_test.py
+++ 01.First-Steps-in-Coding-Lab/handwriting_test.py
@@ -1,6 +1,5 @@
 import pywhatkit
 
-encoding = 'utf-8'
 pywhatkit.text_to_handwriting(
     'To use this data in your Python code you must give a valid utf-8 encoding '
     'declaration at the top of the script, and you also must tell your text editor '
