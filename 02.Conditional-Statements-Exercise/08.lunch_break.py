62b7c6e8dc8d11dcf62631ffa2e9313263d09e7b
vebili <74007431+vebili@users.noreply.github.com>
2022-01-27 11:40:57 +0200

    Up

--- 02.Conditional-Statements-Exercise/08.lunch_break.py
+++ 02.Conditional-Statements-Exercise/08.lunch_break.py
@@ -8,6 +8,8 @@ timeRelax = timeBreak / 4.0
 restBreak = timeBreak - timeLunch - timeRelax
 
 if restBreak >= timeEpisode:
-    print(f"You have enough time to watch {serie} and left with {math.ceil(restBreak - timeEpisode)} minutes free time.")
+    print(f"You have enough time to watch {serie} and left with "
+          f"{math.ceil(restBreak - timeEpisode)} minutes free time.")
 elif restBreak < timeEpisode:
-    print(f"You don't have enough time to watch {serie}, you need {math.ceil(timeEpisode - restBreak)} more minutes.")
+    print(f"You don't have enough time to watch {serie}, you need "
+          f"{math.ceil(timeEpisode - restBreak)} more minutes.")
