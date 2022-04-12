4fe79d78facf474348713a5bc81708ef4986bb54
vebili vbiliarski@gmail.com <vbiliarski@gmail.com>
2022-04-12 11:00:12 +0300

    Up

---
+++ 08.Other/02/jet_brain_02_14.py
@@ -0,0 +1,22 @@
+print("Операции със списък с числа...")
+# Контролируем код:
+try:
+    nums=eval(input("Въведете числов списък: "))
+    print("Получена стойност: "+str(nums))
+    a=int(nums[0])
+    b=int(nums[3])
+    print(str(a)+"/"+str(b)+"="+str(a/b))
+# Обработка на изключения:
+except ValueError:
+    print("ValueError: грешка при преобразуване!")
+except ZeroDivisionError:
+    print("ZeroDivisionError: опит за деление на нула!")
+except TypeError:
+    print("TypeError: недопустима операция!")
+except IndexError:
+    print("IndexError: неверен индекс на елемента!")
+except SyntaxError:
+    print("SyntaxError: невъзможно е изразът да бъде изчислен!")
+except NameError:
+    print("NameError: неверен идентификатор!")
+print("Край на програмата.")
