f2174ab9f2140c87195cb00e00f66184cc0f6b8c
vebili vbiliarski@gmail.com <vbiliarski@gmail.com>
2022-04-14 19:05:58 +0300

    Up

--- 08.Other/02/jet_brain_02_15.py
+++ 08.Other/02/jet_brain_02_15.py
@@ -1,10 +1,10 @@
 print("Операции със списък с числа...")
 try:
-    nums=eval(input("Въведете числов списък: "))
-    print("Получена стойност: "+str(nums))
-    a=int(nums[0])
-    b=int(nums[3])
-    print(str(a)+"/"+str(b)+"="+str(a/b))
+    nums = eval(input("Въведете числов списък: "))
+    print("Получена стойност: " + str(nums))
+    a = int(nums[0])
+    b = int(nums[3])
+    print(str(a) + "/" + str(b) + "=" + str(a / b))
 except ZeroDivisionError:
     print("ZeroDivisionError: опит за деление на нула!")
 except IndexError:
