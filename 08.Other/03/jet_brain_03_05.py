fd3c16b8a3511965cebeea254a5c8a9446a6ed35
vebili vbiliarski@gmail.com <vbiliarski@gmail.com>
2022-04-22 13:34:13 +0300

    Up

---
+++ 08.Other/03/jet_brain_03_05.py
@@ -0,0 +1,14 @@
+Alpha=5*[0]
+print(Alpha)
+Bravo=(1,)*3
+print(Bravo)
+Charlie=[1,2]*3
+print(Charlie)
+Delta=[[1,2]]*3
+print(Delta)
+Echo=4*(1,[2,3])
+print(Echo)
+Foxtrot=([1]*2)*3
+print(Foxtrot)
+Golf=([1]*2,)*3
+print(Golf)
