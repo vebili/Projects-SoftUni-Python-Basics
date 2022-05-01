6692d222c86773f6d2edbd471731723dd6b1f305
vebili vbiliarski@gmail.com <vbiliarski@gmail.com>
2022-05-01 11:43:40 +0300

    Up

--- 08.Other/04/exercise_04_02.py
+++ 08.Other/04/exercise_04_02.py
@@ -1,13 +1,14 @@
 # Включване на функция за генериране на случайни числа:
 from random import *
+
 # Инициализация на генератор на случайни числа:
 seed(2019)
 # Брой различни случайни числа:
-count=10
+count = 10
 # Създаване на празно множество:
-nums=set()
+nums = set()
 # Генериране на случайни числа:
-while len(nums)<count:
-    nums.add(randint(1,count+5))
+while len(nums) < count:
+    nums.add(randint(1, count + 5))
 # Показване на резултата:
 print(nums)
