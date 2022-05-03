bd21972606ae109941652310b1fcdbc2248e9ce3
vebili vbiliarski@gmail.com <vbiliarski@gmail.com>
2022-05-03 10:45:53 +0300

    Up

---
+++ 08.Other/04/exercise_04_06.py
@@ -0,0 +1,19 @@
+# Изходни множества:
+A={1,2}
+B={1,2,3}
+C={3,2,1}
+# Съдържание на множествата:
+print("A =",A)
+print("B =",B)
+print("C =",C)
+# Сравнение на множествата:
+print("A==B:",A==B)
+print("A!=B:",A!=B)
+print("B==C:",B==C)
+print("B!=C:",B!=C)
+print("A<B:",A<B)
+print("A>B:",A>B)
+print("B<C:",B<C)
+print("B<=C:",B<=C)
+print("B>=C:",B>=C)
+print("B>=A:",B>=A)
