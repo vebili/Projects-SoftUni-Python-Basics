cc549a2a189dc320d9eac36b243254060574978a
vebili <74007431+vebili@users.noreply.github.com>
2022-05-18 10:12:16 +0300

    Upd

---
+++ 08.Other/05/exercise_05_08.py
@@ -0,0 +1,10 @@
+txt="Ние изучаваме езика Python"
+print(txt)
+A=txt.replace(" ","_*_")
+print(A)
+B=txt.replace(" ","\n")
+print(B)
+C=txt.replace(" "," не ",1).replace("Python","Java")
+print(C)
+D=txt.replace("езика ","")
+print(D)
