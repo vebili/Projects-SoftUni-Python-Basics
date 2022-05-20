c5de83327e6e34a99b26a83b1efe851003ad7d24
vebili vbiliarski@gmail.com <vbiliarski@gmail.com>
2022-05-20 15:49:53 +0300

    Up

---
+++ 08.Other/05/exercise_05_09.py
@@ -0,0 +1,13 @@
+A=["Alpha","Bravo","Charlie"]
+print("Списък:",A)
+B=", ".join(A)
+print("Текст:",B)
+C=B.split(", ")
+print("Отново списък:",C)
+txt="""Минаха години
+И в светлината на годините
+Има мъдрост
+Но  щастие няма"""
+print(txt)
+D=txt.splitlines()
+print(D)
