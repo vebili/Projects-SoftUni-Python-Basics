8e2d89eeefad041e637f17e416d2369c6e843368
vebili <74007431+vebili@users.noreply.github.com>
2022-05-21 13:45:48 +0300

    Upd

--- 08.Other/05/exercise_05_10.py
+++ 08.Other/05/exercise_05_10.py
@@ -1,17 +1,17 @@
-txt="PYTHON"
-num=20
-A=txt.ljust(num,"_")
-B=txt.center(num)
-C=txt.rjust(num,"*")
-D=txt.center(num,"*")
-E=txt.center(num,"_")
-print("|",A,"|")
-print("")
-print("|",B,"|")
-print()
-print("|",C,"|")
-print('')
-print("|",D,"|","\n")
-print("|",E,"|")
-# print empty line: print() or print('') or print("") or + ~,"\n") at the end of previous printed line.
-
+txt = "PYTHON"
+num = 20
+A = txt.ljust(num, "_")
+B = txt.center(num)
+C = txt.rjust(num, "*")
+D = txt.center(num, "*")
+E = txt.center(num, "_")
+print("|", A, "|")
+# print("")  # print empty line
+print("|", B, "|")
+# print()  # print empty line
+print("|", C, "|")
+# print('')  # print empty line
+print("|", D, "|", "\n")  # print empty line
+print("|", E, "|")
+# print empty line: print() or print('') or print("") or + ~,"\n")
+# at the end of previous printed line.
