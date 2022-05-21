8f9a3ac6f6b401cb2c0fbcf498077e239f7a69fd
vebili <74007431+vebili@users.noreply.github.com>
2022-05-21 13:41:28 +0300

    Upd

---
+++ 08.Other/05/exercise_05_10.py
@@ -0,0 +1,17 @@
+txt="PYTHON"
+num=20
+A=txt.ljust(num,"_")
+B=txt.center(num)
+C=txt.rjust(num,"*")
+D=txt.center(num,"*")
+E=txt.center(num,"_")
+print("|",A,"|")
+print("")
+print("|",B,"|")
+print()
+print("|",C,"|")
+print('')
+print("|",D,"|","\n")
+print("|",E,"|")
+# print empty line: print() or print('') or print("") or + ~,"\n") at the end of previous printed line.
+
