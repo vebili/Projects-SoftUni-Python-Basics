814aa1ff8d6463df1ebeadd6e2298ecc9a9cd2d6
vebili <74007431+vebili@users.noreply.github.com>
2022-05-28 08:54:38 +0300

    Upd

---
+++ 08.Other/06/exercise_06_04.py
@@ -0,0 +1,9 @@
+# Функция с три аргумента:
+def show(first,second,third):
+    print(f"[1] Първи аргумент - {first}")
+    print(f"[2] Втори аргумент - {second}")
+    print(f"[3] Трети аргумент - {third}")
+# Извикване на функцията:
+show(1,2,3)
+show(second="B",third="C",first="A")
+show(1,third=3,second=2)
