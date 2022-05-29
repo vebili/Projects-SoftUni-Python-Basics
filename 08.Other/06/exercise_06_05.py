2ac15622115b9f61c89b3c534e7eec6efb803915
vebili <74007431+vebili@users.noreply.github.com>
2022-05-29 10:02:25 +0300

    Upd

--- 08.Other/06/exercise_06_05.py
+++ 08.Other/06/exercise_06_05.py
@@ -1,22 +1,26 @@
 # Функции:
 def shift(val):
     print("Функция shift()")
-    print("Изходна стойност:",val)
-    val=["A","B","C"]
-    print("Крайна стойност:",val)
+    print("Изходна стойност:", val)
+    val = ["A", "B", "C"]
+    print("Крайна стойност:", val)
+
+
 def change(val):
     print("Функция change()")
-    print("Изходна стойност:",val)
-    if type(val)==list:
+    print("Изходна стойност:", val)
+    if type(val) == list:
         for k in range(len(val)):
-            val[k]+=1
+            val[k] += 1
     else:
-        val+=1
-    print("Крайна стойност:",val)
+        val += 1
+    print("Крайна стойност:", val)
+
+
 # Променлива:
-num=100
+num = 100
 # Списък:
-L=[10,20,30]
+L = [10, 20, 30]
 # Извикване на функциите:
 print(f"Променлива num={num}")
 change(num)
