10d1efe84488c4f5e622e6006d0327c9876290cc
vebili <74007431+vebili@users.noreply.github.com>
2022-06-01 14:26:05 +0300

    Upd

--- 08.Other/06/exercise_06_07.py
+++ 08.Other/06/exercise_06_07.py
@@ -1,8 +1,10 @@
 # Стойността по подразбиране за аргумента е списък:
-def show(val=[0,1,2]):
+def show(val=[0, 1, 2]):
     for k in range(len(val)):
-       val[k]+=1;
+        val[k] += 1
     print(val)
+
+
 # Извикване на функцията:
 show()
 show()
