0c5205ec0c0510066c2b0334132cc757bd9924c0
vebili <74007431+vebili@users.noreply.github.com>
2022-06-02 08:10:17 +0300

    Upd

--- 08.Other/06/exercise_06_08.py
+++ 08.Other/06/exercise_06_08.py
@@ -1,22 +1,28 @@
 # Функции с произволно количество аргументи:
 def sqr_sum(*n):
-    s=0
+    s = 0
     for a in n:
-       s+=a*a
+        s += a * a
     return s
+
+
 def get_sum(*n):
-    s=0
+    s = 0
     for a in n:
-        if type(a)==int:
-           s+=a
+        if type(a) == int:
+            s += a
     return s
+
+
 def get_text(*t):
     return " ".join(t)
+
+
 # Извикване на функциите:
-print("Сума на квадратите:",sqr_sum(1,3,5))
-print("Сума на квадратите:",sqr_sum(2,4,6,8,10))
-print("Сума на числата:",get_sum(2,"A",4,"B",6))
-print("Сума на числата:",get_sum(1,[2,3],4))
-print("Сума на числата:",get_sum())
-print("Текст:",get_text("Привет","на всички"))
-print("Текст:",get_text("A","B","C","D"))
+print("Сума на квадратите:", sqr_sum(1, 3, 5))
+print("Сума на квадратите:", sqr_sum(2, 4, 6, 8, 10))
+print("Сума на числата:", get_sum(2, "A", 4, "B", 6))
+print("Сума на числата:", get_sum(1, [2, 3], 4))
+print("Сума на числата:", get_sum())
+print("Текст:", get_text("Привет", "на всички"))
+print("Текст:", get_text("A", "B", "C", "D"))
