f5d534508c0fe5f5128657e8be9b97a6fde27674
vebili <74007431+vebili@users.noreply.github.com>
2022-06-09 08:25:51 +0300

    Upd

--- 08.Other/06/exercise_06_18.py
+++ 08.Other/06/exercise_06_18.py
@@ -1,10 +1,12 @@
 # Функция с анотации:
-def show(txt:"Текст"="Функция show()")->"Няма резултат":
+def show(txt: "Текст" = "Функция show()") -> "Няма резултат":
     print(txt)
+
+
 # Извикване на функцията:
 show()
 # Речник с анотациите:
 print(show.__annotations__)
 # Анотации:
 for k in show.__annotations__:
-    print(k,"-",show.__annotations__[k])
+    print(k, "-", show.__annotations__[k])
