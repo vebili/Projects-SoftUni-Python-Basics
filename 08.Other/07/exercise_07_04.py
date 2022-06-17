e459184f126df79fff49034f61e7e86e336ca9d5
vebili <74007431+vebili@users.noreply.github.com>
2022-06-17 11:01:15 +0300

    Upd

---
+++ 08.Other/07/exercise_07_04.py
@@ -0,0 +1,14 @@
+# Комплексни числа:
+A=3+4j
+print("A =",A)
+print("Re(A) =",A.real)
+print("Im(A) =",A.imag)
+print("|A| =",abs(A))
+B=-1+1j
+print("B =",B)
+C=complex(0,1)
+print("C =",C)
+# Аритметични операции:
+print("A+B =",A+B)
+print("A*C =",A*C)
+print("A/B =",A/B)
