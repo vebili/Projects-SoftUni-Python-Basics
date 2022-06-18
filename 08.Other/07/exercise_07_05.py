8220676f3a177770d5d02175ca423bd865a187d2
vebili <74007431+vebili@users.noreply.github.com>
2022-06-18 09:14:51 +0300

    Upd

---
+++ 08.Other/07/exercise_07_05.py
@@ -0,0 +1,12 @@
+A=123
+print("A =",A)
+B="Python"
+print("B =",B)
+C=[]
+print("C =",C)
+D=0
+print("D =",D)
+print("A or B:",A or B)
+print("A and B:",A and B)
+print("C or D:",C or D)
+print("C and D:",C and D)
