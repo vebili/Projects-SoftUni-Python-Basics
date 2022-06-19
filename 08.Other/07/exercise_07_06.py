23da8ec92cff51b14754b0b2fbe80bc32255b651
vebili <74007431+vebili@users.noreply.github.com>
2022-06-19 08:53:28 +0300

    Upd

--- 08.Other/07/exercise_07_06.py
+++ 08.Other/07/exercise_07_06.py
@@ -1,18 +1,19 @@
 # Импортиране на класа от модула:
 from datetime import time
+
 # Обект за реализация на момент от време:
-mytime=time(13,35,20)
+mytime = time(13, 35, 20)
 # Проверка на резултата:
-print("Време:",mytime)
+print("Време:", mytime)
 # Използване на полетата на обекта:
-print("Часове:",mytime.hour)
-print("Минути:",mytime.minute)
-print("Секунди:",mytime.second)
+print("Часове:", mytime.hour)
+print("Минути:", mytime.minute)
+print("Секунди:", mytime.second)
 # Създаване на нов обект на основата на съществуващ:
-newtime=mytime.replace(15,second=45)
+newtime = mytime.replace(15, second=45)
 # Проверка на резултата:
-print("Време:",newtime)
+print("Време:", newtime)
 # Създаване на нов обект:
-mytime=time.fromisoformat("12:34:56")
+mytime = time.fromisoformat("12:34:56")
 # Проверка на резултата:
-print("Време:",mytime)
+print("Време:", mytime)
