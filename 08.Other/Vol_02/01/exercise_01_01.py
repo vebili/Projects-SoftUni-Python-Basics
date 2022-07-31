5562913cd54d2ac7df79c11fe00aa89321370ff0
vebili vbiliarski@gmail.com <vbiliarski@gmail.com>
2022-07-31 14:27:01 +0300

    Up

--- 08.Other/Vol_02/01/exercise_01_01.py
+++ 08.Other/Vol_02/01/exercise_01_01.py
@@ -1,14 +1,16 @@
 # Описание на класа:
 class MyClass:
     pass
+
+
 # Създаване на обекти на основата на класа:
-A=MyClass()
-B=MyClass()
+A = MyClass()
+B = MyClass()
 # Обекти:
-print("Обект A:",A)
-print("Обект B:",B)
+print("Обект A:", A)
+print("Обект B:", B)
 # Тип на обектите:
-print("Клас на обект A:",type(A).__name__)
-print("Клас на обект B:",type(B).__name__)
+print("Клас на обект A:", type(A).__name__)
+print("Клас на обект B:", type(B).__name__)
 # Сравнение на обекти:
-print("A==B:",A==B)
+print("A==B:", A == B)
