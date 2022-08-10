d0151b2565fdd5524656a98c0f8047d3bee25f63
vebili vbiliarski@gmail.com <vbiliarski@gmail.com>
2022-08-10 09:14:06 +0300

    Up

--- 08.Other/Vol_02/01/exercise_01_04.py
+++ 08.Other/Vol_02/01/exercise_01_04.py
@@ -1,23 +1,28 @@
 # Описание на класа:
 class MyClass:
     # Конструктор:
-    def __init__(self,n="Бял"):
-        self.name=n
-        print("Създаден е обект:",self.name)
+    def __init__(self, n="Бял"):
+        self.name = n
+        print("Създаден е обект:", self.name)
+
     # Деструктор:
     def __del__(self):
-        print("Изтрит е обект:",self.name)
+        print("Изтрит е обект:", self.name)
+
+
 # Функция:
 def create(n):
-    obj=MyClass(n)
+    obj = MyClass(n)
+
+
 # Създаване на обекти:
-A=MyClass()
-B=MyClass("Червен")
-C=MyClass("Син")
+A = MyClass()
+B = MyClass("Червен")
+C = MyClass("Син")
 # Извикване на функция:
 create("Жълт")
 # На полето се присвоява нова стойност:
-C.name="Зелен"
+C.name = "Зелен"
 # Изтриване на обектите:
 del A
 del B
