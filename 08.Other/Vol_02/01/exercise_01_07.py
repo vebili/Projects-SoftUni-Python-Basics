b410a961b303a57385a98b6f3e8d50d2911b3381
vebili vbiliarski@gmail.com <vbiliarski@gmail.com>
2022-08-13 15:37:24 +0300

    Up

--- 08.Other/Vol_02/01/exercise_01_07.py
+++ 08.Other/Vol_02/01/exercise_01_07.py
@@ -2,25 +2,30 @@
 class MyClass:
     # Конструктор:
     def __init__(self):
-        self.value=123
-        print("Създава се обект:",self.value)
+        self.value = 123
+        print("Създава се обект:", self.value)
+
     # Деструктор:
     def __del__(self):
-        print("Изтрива се обект:",self.value)
+        print("Изтрива се обект:", self.value)
+
     # Метод за присвояване на стойност на полето:
-    def set(self,n):
-        self.value=n
+    def set(self, n):
+        self.value = n
+
     # Метод за показване на стойността на полето:
     def show(self):
-        print("Поле на обекта:",self.value)
+        print("Поле на обекта:", self.value)
+
+
 # Създаване на обект:
-obj=MyClass()
+obj = MyClass()
 # Извикване на методите от обекта:
 obj.show()
 obj.set(100)
 # Извикване на методите от класа:
 MyClass.show(obj)
-MyClass.set(obj,200)
+MyClass.set(obj, 200)
 # Проверка на стойността на полето:
 obj.show()
 # Явно извикване на конструктора:
@@ -30,7 +35,7 @@ MyClass.__del__(obj)
 # Проверка на стойността на полето:
 obj.show()
 # Изменение на стойността на полето:
-obj.value=321
+obj.value = 321
 obj.show()
 # Явно извикване на конструктора чрез обекта:
 obj.__init__()
