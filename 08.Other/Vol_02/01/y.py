71f4f0b366fae0eab365e499d30f6cf60f3ab380
vebili <74007431+vebili@users.noreply.github.com>
2022-08-26 11:36:00 +0300

    Upd

--- 08.Other/Vol_02/01/y.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-integers = [int(num) for num in input().split()]
-
-integers_to_remove = max(integers)
-
-while integers_to_remove in integers:
-    integers.remove(integers_to_remove)
-
-print(max(integers))
