f0742810d928108c4f613528f63acfa0a97f0539
vebili vbiliarski@gmail.com <vbiliarski@gmail.com>
2022-09-10 17:12:17 +0300

    Up

--- 08.Other/Vol_02/01/exercise_01_10.py
+++ 08.Other/Vol_02/01/exercise_01_10.py
@@ -2,19 +2,23 @@
 class Alpha:
     """Това е клас Alpha"""
     pass
+
+
 # Втори клас:
 class Bravo:
     """Това е клас Bravo"""
     pass
+
+
 # Информация за класовете:
 print(Alpha.__doc__)
 print(Bravo.__doc__)
 # Обекти на класовете:
-A=Alpha()
-B=Bravo()
+A = Alpha()
+B = Bravo()
 # Изменение на документиращия низ:
-Alpha.__doc__="Първи клас"
-B.__class__.__doc__="Втори клас"
+Alpha.__doc__ = "Първи клас"
+B.__class__.__doc__ = "Втори клас"
 # Информация за класовете:
 print(A.__class__.__doc__)
 print(B.__doc__)
