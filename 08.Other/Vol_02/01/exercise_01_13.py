b21feb429c78aec485c9cfaea7a8247679d317c1
vebili vbiliarski@gmail.com <vbiliarski@gmail.com>
2022-09-22 17:56:23 +0300

    Up

--- 08.Other/Vol_02/01/exercise_01_13.py
+++ 08.Other/Vol_02/01/exercise_01_13.py
@@ -1,42 +1,45 @@
 # Описание на клас:
 class MyClass:
     # Конструктор:
-    def __init__(self,val):
+    def __init__(self, val):
         # Ако аргументът е целочислен:
-        if type(val)==int:
-            self.number=val
+        if type(val) == int:
+            self.number = val
         # Ако аргументът е текстов:
-        elif type(val)==str:
-            self.name=val
+        elif type(val) == str:
+            self.name = val
         # Ако аргументът е реално число:
-        elif type(val)==float:
-            self.value=val
+        elif type(val) == float:
+            self.value = val
         # Останалите случаи:
         else:
-            self.data=val
+            self.data = val
+
     # Метод за показване на стойността на полето:
     def show(self):
         # Списък с имената на полетата:
-        L=["number","name","value","data"]
+        L = ["number", "name", "value", "data"]
         # Обхождане на имената на полетата:
         for s in L:
             # Ако полето съществува:
             if s in dir(self):
                 # Показване на името и стойността на полето:
-                print(s,"=",self.__dict__[s])
+                print(s, "=", self.__dict__[s])
                 # Край на оператора за цикъл:
                 break
         # Ако полето не е намерено:
         else:
             print("Странен обект")
+
+
 # Създаване на обекти и проверка на полетата:
-A=MyClass(123)
+A = MyClass(123)
 A.show()
 del A.number
 A.show()
-B=MyClass("Обект B")
+B = MyClass("Обект B")
 B.show()
-C=MyClass(2.5)
+C = MyClass(2.5)
 C.show()
-D=MyClass([1,2,3])
+D = MyClass([1, 2, 3])
 D.show()
