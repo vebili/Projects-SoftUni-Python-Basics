5041a5e04576f8e26bc0d83c54bbb9a3c204669c
vebili vbiliarski@gmail.com <vbiliarski@gmail.com>
2022-09-26 15:49:48 +0300

    Up

--- 08.Other/Vol_02/01/exercise_01_15.py
+++ 08.Other/Vol_02/01/exercise_01_15.py
@@ -1,33 +1,38 @@
 # Клас:
 class MyClass:
     # Конструктор:
-    def __init__(self,name,n=1):
-        self.name=name
-        if n==1:
-            self.next=None
+    def __init__(self, name, n=1):
+        self.name = name
+        if n == 1:
+            self.next = None
         else:
-            self.next=MyClass(self.name,n-1)
+            self.next = MyClass(self.name, n - 1)
         self.set()
+
     # Деструктор:
     def __del__(self):
-        print("Изтриване:",self.code)
+        print("Изтриване:", self.code)
+
     # Метод за запълване на веригата с кодове:
-    def set(self,num=1):
-        self.code=self.name+"["+str(num)+"]"
-        if self.next!=None:
-            self.next.set(num+1)
-     # Метод за показване на кодовете на обектите 
-     # във веригата:
+    def set(self, num=1):
+        self.code = self.name + "[" + str(num) + "]"
+        if self.next != None:
+            self.next.set(num + 1)
+
+    # Метод за показване на кодовете на обектите
+    # във веригата:
     def show(self):
         print(self.code)
-        if self.next!=None:
+        if self.next != None:
             self.next.show()
+
+
 # Създаване на верига обекти:
 print("Един обект:")
-A=MyClass("Alpha")
+A = MyClass("Alpha")
 A.show()
 print("Верига обекти:")
-B=MyClass("Bravo",5)
+B = MyClass("Bravo", 5)
 B.show()
 print("Започвайки от третия обект:")
 B.next.next.show()
