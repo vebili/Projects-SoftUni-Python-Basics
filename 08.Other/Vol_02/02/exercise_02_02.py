bd4f6f45a4735070eabc02c69dd9b98cc70d27c8
vebili vbiliarski@gmail.com <vbiliarski@gmail.com>
2022-10-02 12:01:33 +0300

    Up

--- 08.Other/Vol_02/02/exercise_02_02.py
+++ 08.Other/Vol_02/02/exercise_02_02.py
@@ -1,30 +1,39 @@
 # Първи клас:
 class Alpha:
-    code=123
+    code = 123
+
     def alpha(self):
-        print("Alpha:",self.code)
+        print("Alpha:", self.code)
+
+
 # Втори клас:
 class Bravo(Alpha):
     def bravo(self):
-        print("Bravo:",self.code)
+        print("Bravo:", self.code)
+
+
 # Трети клас:
 class Charlie(Bravo):
     def charlie(self):
-        print("Charlie:",self.code)
+        print("Charlie:", self.code)
+
+
 # Функция за показване на йерархията на наследяването:
 def show(MyClass):
-    print("Класс",MyClass.__name__,end=":\n")
+    print("Класс", MyClass.__name__, end=":\n")
     for s in MyClass.__mro__:
-        print("<",s.__name__,">",end="",sep="")
+        print("<", s.__name__, ">", end="", sep="")
     print()
+
+
 # Йерархия на наследяването на класовете:
 show(Alpha)
 show(Bravo)
 show(Charlie)
 # Създаване на обекти:
-A=Alpha()
-B=Bravo()
-C=Charlie()
+A = Alpha()
+B = Bravo()
+C = Charlie()
 # Извикване на методи:
 print("Обект A")
 A.alpha()
@@ -36,7 +45,7 @@ C.alpha()
 C.bravo()
 C.charlie()
 # Присвояване на стойност на полето:
-Bravo.code=321
+Bravo.code = 321
 print("Изпълнена е командата Bravo.code=321")
 # Извикване на методи:
 print("Обект C")
