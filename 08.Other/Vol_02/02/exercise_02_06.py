da78b9cac0350df1f88aa6d766dc1b7500b9aca3
vebili vbiliarski@gmail.com <vbiliarski@gmail.com>
2022-10-03 10:43:33 +0300

    Up

--- 08.Other/Vol_02/02/exercise_02_06.py
+++ 08.Other/Vol_02/02/exercise_02_06.py
@@ -3,20 +3,25 @@ class Alpha:
     # Методи:
     def display(self):
         print("Метод от Alpha")
-        print("Поле code:",self.code)
+        print("Поле code:", self.code)
+
     def show(self):
         self.display()
+
+
 # Производен клас:
 class Bravo(Alpha):
     # Предефиниране на метода:
     def display(self):
         print("Метод от Bravo")
-        print("Поле name:",self.name)
+        print("Поле name:", self.name)
+
+
 # Създаване на обекти:
-A=Alpha()
-A.code=123
-B=Bravo()
-B.name="B"
+A = Alpha()
+A.code = 123
+B = Bravo()
+B.name = "B"
 # Извикване на методите:
 A.show()
 B.show()
