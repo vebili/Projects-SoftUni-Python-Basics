f95608f77e27a791c5d0a14870840f77347f3321
vebili vbiliarski@gmail.com <vbiliarski@gmail.com>
2022-10-05 20:00:49 +0300

    Up

--- 08.Other/Vol_02/02/exercise_02_08.py
+++ 08.Other/Vol_02/02/exercise_02_08.py
@@ -1,49 +1,60 @@
 # Първи клас:
 class Alpha:
     # Конструктор:
-    def __init__(self,num):
-        self.code=num
+    def __init__(self, num):
+        self.code = num
+
     # Метод:
     def show(self):
-        print("Клас Alpha:",self.code)
+        print("Клас Alpha:", self.code)
+
+
 # Втори клас:
 class Bravo(Alpha):
     # Предефиниране на метода:
     def show(self):
-        print("Клас Bravo:",self.code)
+        print("Клас Bravo:", self.code)
         super().show()
+
+
 # Трети клас:
 class Charlie(Alpha):
     # Предефиниране на метода:
     def show(self):
-        print("Клас Charlie:",self.code)
-        super(Charlie,self).show()
+        print("Клас Charlie:", self.code)
+        super(Charlie, self).show()
+
+
 # Четвърти клас:
-class Delta(Bravo,Charlie):
+class Delta(Bravo, Charlie):
     # Предефиниране на метода:
     def show(self):
-        print("Клас Delta:",self.code)
+        print("Клас Delta:", self.code)
         super().show()
         Charlie.show(self)
-        super(Bravo,self).show()
+        super(Bravo, self).show()
+
+
 # Функция за показване на веригата от наследявания:
 def display(MyClass):
-    print("Наследяване за "+MyClass.__name__+":")
-    k=1
+    print("Наследяване за " + MyClass.__name__ + ":")
+    k = 1
     for s in MyClass.__mro__:
-        print("["+str(k)+"]",s.__name__)
-        k+=1
+        print("[" + str(k) + "]", s.__name__)
+        k += 1
+
+
 # Показване на веригата от наследявания,
 # създаване на обекти и извикване на метода:
 display(Alpha)
-A=Alpha(100)
+A = Alpha(100)
 A.show()
 display(Bravo)
-B=Bravo(200)
+B = Bravo(200)
 B.show()
 display(Charlie)
-C=Charlie(300)
+C = Charlie(300)
 C.show()
 display(Delta)
-D=Delta(400)
+D = Delta(400)
 D.show()
