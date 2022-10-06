837f630b090a66ae87c33e03fab431adff96ffe5
vebili <74007431+vebili@users.noreply.github.com>
2022-10-06 22:35:53 +0300

    Upd

--- 08.Other/Vol_02/02/exercise_02_04.py
+++ 08.Other/Vol_02/02/exercise_02_04.py
@@ -1,16 +1,26 @@
 # Описание на класове:
 class Alpha:
     pass
+
+
 class Bravo:
     pass
-class Charlie(Alpha,Bravo):
+
+
+class Charlie(Alpha, Bravo):
     pass
+
+
 class Delta(Bravo):
     pass
-class Echo(Charlie,Delta):
+
+
+class Echo(Charlie, Delta):
     pass
+
+
 # Йерархия от наследявания:
-k=1
+k = 1
 for s in Echo.__mro__:
-   print("["+str(k)+"]",s.__name__)
-   k+=1
+    print("[" + str(k) + "]", s.__name__)
+    k += 1
