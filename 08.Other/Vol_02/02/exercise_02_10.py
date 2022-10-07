1294cfd4b4b8673e14832606c8c49ac610858d85
vebili <74007431+vebili@users.noreply.github.com>
2022-10-07 13:15:08 +0300

    Upd

--- 08.Other/Vol_02/02/exercise_02_10.py
+++ 08.Other/Vol_02/02/exercise_02_10.py
@@ -1,24 +1,28 @@
 # Клас със специални методи:
 class MyClass:
     # Конструктор:
-    def __init__(self,num):
-        self.code=num
+    def __init__(self, num):
+        self.code = num
+
     # Метод за преобразуване в текстов формат:
     def __str__(self):
         return str(self.code)
+
     # Метод за презареждане на оператора за събиране:
-    def __add__(self,n):
-        if type(n)==int:
-            val=self.code+n
+    def __add__(self, n):
+        if type(n) == int:
+            val = self.code + n
         else:
-            val=0
+            val = 0
         return MyClass(val)
+
+
 # Създаване на обект и проверка на методите:
-A=MyClass(100)
-print("Обект A:",A)
+A = MyClass(100)
+print("Обект A:", A)
 # Към обекта се прибавя число:
-B=A+25
-print("Обект B:",B)
+B = A + 25
+print("Обект B:", B)
 # Към обекта се прибавя текст:
-C=A+"Hello"
-print("Обект C:",C)
+C = A + "Hello"
+print("Обект C:", C)
