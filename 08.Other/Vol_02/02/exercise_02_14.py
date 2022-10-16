1d22c1f764a8841aa94714a638f94996d7db2dd5
vebili vbiliarski@gmail.com <vbiliarski@gmail.com>
2022-10-16 17:49:08 +0300

    Up

--- 08.Other/Vol_02/02/exercise_02_14.py
+++ 08.Other/Vol_02/02/exercise_02_14.py
@@ -1,28 +1,33 @@
 # Клас със специален метод:
 class Alpha:
     # Методът се извиква, ако атрибутът не съществува:
-    def __getattr__(self,name):
+    def __getattr__(self, name):
         return len(name)
+
+
 # Клас със специален метод:
 class Bravo:
     # Обикновен метод:
-    def show(self,x):
-        print("Метод show():",x)
+    def show(self, x):
+        print("Метод show():", x)
+
     # Методът се извиква, ако атрибутът не съществува:
-    def __getattr__(self,name):
-        if len(name)<5:
-            return lambda x: print("Първи метод:",x)
+    def __getattr__(self, name):
+        if len(name) < 5:
+            return lambda x: print("Първи метод:", x)
         else:
-            return lambda x: print("Втори метод:",x*x)
+            return lambda x: print("Втори метод:", x * x)
+
+
 # Създаване на обекти и обръщане към атрибутите им:
 print("Обект A на класа Alpha")
-A=Alpha()
-A.value="Alpha"
-print("Поле value:",A.value)
-print("Поле color:",A.color)
-print("Поле myattribute:",A.myattribute)
+A = Alpha()
+A.value = "Alpha"
+print("Поле value:", A.value)
+print("Поле color:", A.color)
+print("Поле myattribute:", A.myattribute)
 print("Обект B на класа Bravo")
-B=Bravo()
+B = Bravo()
 B.show(10)
 B.hi(10)
 B.display(10)
