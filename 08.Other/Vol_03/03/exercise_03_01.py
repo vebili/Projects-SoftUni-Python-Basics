1346bde3645203af59971636679418452892c33a
vebili <74007431+vebili@users.noreply.github.com>
2023-01-31 14:29:46 +0200

    Upd

--- 08.Other/Vol_03/03/exercise_03_01.py
+++ 08.Other/Vol_03/03/exercise_03_01.py
@@ -1,20 +1,26 @@
 import heapq
 
+
 class PQ:
-  def __init__(self):
-      self._queue = []
-      self._index = 0
-  def push(self, item, priority):
-      heapq.heappush(self._queue, (-priority, self._index, item))
-      self._index += 1
-  def pop(self):
-      return heapq.heappop(self._queue)[-1]
+    def __init__(self):
+        self._queue = []
+        self._index = 0
+
+    def push(self, item, priority):
+        heapq.heappush(self._queue, (-priority, self._index, item))
+        self._index += 1
+
+    def pop(self):
+        return heapq.heappop(self._queue)[-1]
+
 
 class Item:
-      def __init__(self, name):
-           self.name = name
-      def __repr__(self):
-           return 'Item({!r})'.format(self.name)
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return 'Item({!r})'.format(self.name)
+
 
 q = PQ()
 q.push(Item('test'), 1)
